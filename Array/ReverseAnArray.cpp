a4aa9c524268c990904f0c7dcba0ad182e1307ab
Priyanka Gaikwad <166126386+Priyankagaikwad2004@users.noreply.github.com>
2025-06-21 19:38:58 +0530

    Update ReverseAnArray.cpp

--- Array/ReverseAnArray.cpp
+++ Array/ReverseAnArray.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+void reverseArray(vector<int>& arr) {
+    int start = 0, end = arr.size() - 1;
+    while(start < end) {
+        swap(arr[start++], arr[end--]);
+    }
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    reverseArray(arr);
+    for(int i : arr) cout << i << " ";
+    return 0;
+}
